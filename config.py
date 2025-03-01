cda5f9940d8575499ba7160696e3e17959717c19
chandanhegde7 <chandanhegde7@gmail.com>
2025-03-01 14:09:11 +0530

    changes to the config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_KEY = 39078895019bb9e70c99c6f53e0a878591d66d67ccb84bde944cfeb1ad624eec
+API_KEY = "39078895019bb9e70c99c6f53e0a878591d66d67ccb84bde944cfeb1ad624eec"
 BASE_URL = "https://www.virustotal.com/api/v3/"
